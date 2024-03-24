7b1442af8a242bd7b1f6eb02f2e351facc3a5eba
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-25 00:21:01 +0800

    Update handleCommandEvent.js

--- ALVI/system/handle/handleCommandEvent.js
+++ ALVI/system/handle/handleCommandEvent.js
@@ -1,5 +1,5 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    const logger = require("../../catalogs/hamimc.js")
+    const logger = require("../../catalogs/alvic.js")
     return function ({ event }) {
         const { allowInbox } = global.ryuko;
         const { userBanned, threadBanned } = global.data;

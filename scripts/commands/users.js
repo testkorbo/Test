05d329c5cd013e73612b5a551dbc05dbb985e2bd
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-25 10:48:30 +0800

    Update users.js

--- scripts/commands/users.js
+++ scripts/commands/users.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "user",
 	version: "1.0.5",
 	permission: 2,
-	credits: "Hamim",
+	credits: "ALVI",
 	prefix: true,
   premium: false,
 	description: "ban or unblock users",

b5cbd424b87701b984892cfa8551f76e7372d7d4
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-25 13:22:54 +0800

    Create leave.js

---
+++ scripts/events/Alvi/leave.js
@@ -0,0 +1,34 @@
+module.exports.config = {
+	name: "leave",
+	eventType: ["log:unsubscribe"],
+	version: "1.0.0",
+	credits: "Mirai Team",
+	description: "Notify About Members Removed Or That Have Left The Group.",
+	dependencies: {
+		"fs-extra": "",
+		"path": ""
+	}
+};
+
+module.exports.run = async function({ api, event, Users, Threads }) {
+	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+	const { join } =  global.nodemodule["path"];
+	const { threadID } = event;
+	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "leave☹️" : "";
+	const path = join(__dirname, "Alvi", "leavemp4");
+	const gifPath = join(path, `alvi.mp4`);
+	var msg, formPush
+
+	if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+	(typeof data.customLeave == "undefined") ? msg = "~ পালাইছে রে পালাইছে🤣.\n{name}\nReason:আহারে চলে গেলো 😾 কিন্তু কই পালাইবি 😹ভিডিও দেখে যা🥵🥱. {type}." : msg = data.customLeave;
+	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+
+	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
+	else formPush = { body: msg }
+	
+	return api.sendMessage(formPush, threadID);
+}

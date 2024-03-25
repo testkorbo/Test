a1135a2e23a5ee42c1dc04de3a20ed10b66d7a9a
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-25 10:06:57 +0800

    Update allgroups.js

--- scripts/commands/allgroups.js
+++ scripts/commands/allgroups.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "allgroups",
   version: "2.0.0",
   permission: 2,
-  credits: "Hamim",
+  credits: "ALVI",
   description: "all groups",
   prefix: true,
   premium: false,

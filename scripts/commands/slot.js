fdefae926ec92c8a4d5fabe9cf8b4d47bd20f893
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-26 14:21:00 +0800

    Update slot.js

--- scripts/commands/slot.js
+++ scripts/commands/slot.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "slot",
     version: "1.0.1",
     permission: 0,
-    credits: "ALVI",
+    credits: "ryuko",
     premium: false,
     prefix: false,
     description: "slot game",

7d8150ebb3f78144166e21bfd2ab153c2a4e3a2f
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-26 14:10:59 +0800

    Update married.js

--- scripts/commands/married.js
+++ scripts/commands/married.js
@@ -71,6 +71,6 @@ module.exports.run = async function ({ event, api, args }) {
   if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
   else {
       const one = senderID, two = mention[0];
-      return makeImage({ one, two }).then(path => api.sendMessage({ body: "•⎯͢⎯⃝🩵🪽.আমার শূন্য আঁধারে পূর্ন তুমি🖤\n\n__তোমার মায়া'তে মুগ্ধ আমি🩷😌💔", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+      return makeImage({ one, two }).then(path => api.sendMessage({ body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n-🦋🌻•\n\n•— সুন্দরি না হক বিশ্বাসি হক জিবন সঙ্গি__❤️‍🩹🫶🏻🥺\n\n-\n╰──────•◈•───────╯", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
   }
     }

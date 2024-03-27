f22ea339f5a33ff631ef1a3fe9985baae4622514
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-27 19:55:57 +0800

    Update commands

---
+++ scripts/commands/cumu dao.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "cumu dao",
+  version: "1.0.0", 
+  permission: 0,
+  credits: "ALVI",
+  description: "", 
+  premium: false,
+  prefix: true,
+  category: "user",
+  usages: "",
+  cooldowns: 5, 
+
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("উম্মম্মাহ")==0 || event.body.indexOf("kiss")==0 ||
+event.body.indexOf("Kiss")==0 ||
+event.body.indexOf("cumu")==0 ||
+event.body.indexOf("Cumu")==0 ||
+event.body.indexOf("কিস")==0 ||
+event.body.indexOf("ummmmmah")==0 || event.body.indexOf("ummmmmmmmmmmmah")==0 || event.body.indexOf("চুমু")==0) {
+		var msg = {
+				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n__ওই সুনো না-🙁\n-একটা উম্মম্মাহ দিবা-🙈🤭💋\n╰──────•◈•───────╯",
+				attachment: fs.createReadStream(__dirname + `/nofrefix/kiss.mp4`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("💋", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }

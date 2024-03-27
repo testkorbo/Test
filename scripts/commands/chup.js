21afb63564a893d776963dd109af9ac1271ef625
MR-ALVI-CHOWDHURY <164474160+ALVIXHOWDHURY88@users.noreply.github.com>
2024-03-27 19:42:13 +0800

    Update chup.js

--- scripts/commands/chup.js
+++ scripts/commands/chup.js
@@ -1,29 +1,31 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Cup",
-    version: "1.0.1",
-	hasPermssion: 0,
-	credits: "VanHung - Fixed by LTD", 
-	description: "hihihihi",
+  name: "chup",
+  version: "1.0.0", 
+  permission: 0,
+  credits: "ALVI",
+  description: "", 
   premium: false,
   prefix: true,
-	commandCategory: "Không cần dấu lệnh",
-	usages: "Cup",
-    cooldowns: 5, 
+  category: "user",
+  usages: "",
+  cooldowns: 5, 
+
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-  let bot = ['61551846081032'];
-	if (event.body.indexOf("bot cup")==0 || event.body.indexOf("Bot cup")==0 || event.body.indexOf("Bot chup")==0 || event.body.indexOf("cup")==0 ||event.body.indexOf("Cup")==0 || event.body.indexOf("Chup")==0 || event.body.indexOf("chup")==0 || event.body.indexOf("চুপ")==0 ||event.body.indexOf("বুট চুপ")==0 || event.body.indexOf("Chup bot")==0 && !bot.includes(event.senderID))  {
+	if (event.body.indexOf("cup")==0 || event.body.indexOf("Cup")==0 ||
+event.body.indexOf("CHUP")==0 ||
+event.body.indexOf("চুপ")==0 || event.body.indexOf("chup")==0 || event.body.indexOf("Chup")==0) {
 		var msg = {
 				body: "╭──────•◈•───────╮\n         🄰🄻🅅🄸🄱🄾🅃       \n-ওই চুপ বেশি বুঝো 🫵তুমি-🤫😒\n╰──────•◈•───────╯",
 				attachment: fs.createReadStream(__dirname + `/noprefix/chup.mp4`)
 			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🤨", event.messageID, (err) => {}, true)
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🤫", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-      }
+  }

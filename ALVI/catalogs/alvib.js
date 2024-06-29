4f40830e55e37edff763e1c7f9d09c6cf200a8f0
MR. ALVI CHOWDHURY <162790795+ALVICHOWDHURY7@users.noreply.github.com>
2024-06-29 10:31:17 +0800

    Update alvib.js

--- ALVI/catalogs/alvib.js
+++ ALVI/catalogs/alvib.js
@@ -272,7 +272,7 @@ function onBot({ models: botModel }) {
 
             if (!config?.category) {
               try {
-                throw new Error(` • HEY X2 •  ${command} category is not in the correct format or empty`);
+                throw new Error(` • HEY ALVI •  ${command} category is not in the correct format or empty`);
               } catch (error) {
                 console.log(chalk.red(error.message));
                 continue;
@@ -281,17 +281,17 @@ function onBot({ models: botModel }) {
             const configures = require(`../../Alvi.json`);
             if (configures.premium) {
               if (!config?.hasOwnProperty('premium')) {
-                console.log(`• HEY X2 • `, chalk.hex("#ff0000")(command) + ` does not have the "premium" property.`);
+                console.log(`• HEY ALVI • `, chalk.hex("#ff0000")(command) + ` does not have the "premium" property.`);
                 continue;
               }
             }
             if (!config?.hasOwnProperty('prefix')) {
-              console.log(`• HEY X2 • `, chalk.hex("#ff0000")(command) + ` does not have the "prefix" property.`);
+              console.log(`• HEY ALVI • `, chalk.hex("#ff0000")(command) + ` does not have the "prefix" property.`);
               continue;
             }
 
             if (global.client.commands.has(config.name || '')) {
-              console.log(chalk.red(`• HEY X2 •  ${chalk.hex("#FFFF00")(command)} module is already deployed.`));
+              console.log(chalk.red(`• HEY ALVI •  ${chalk.hex("#FFFF00")(command)} module is already deployed.`));
               continue;
             }
             const { dependencies, envConfig } = config;
@@ -362,7 +362,7 @@ function onBot({ models: botModel }) {
             const event = require(join(global.client.mainPath, '../../scripts/events', ev));
             const { config, onLoad, run } = event;
             if (!config || !config.name || !run) {
-              global.loading.err(`${chalk.hex('#ff7100')(``)} ${chalk.hex("#FFFF00")(ev)} module is not in the correct format. `, "• X2 EVENT •");
+              global.loading.err(`${chalk.hex('#ff7100')(``)} ${chalk.hex("#FFFF00")(ev)} module is not in the correct format. `, "• ALVI EVENT •");
               continue;
             }
 
@@ -375,7 +375,7 @@ function onBot({ models: botModel }) {
             }
 
             if (global.client.events.has(config.name)) {
-              global.loading.err(`${chalk.hex('#ff7100')(``)} ${chalk.hex("#FFFF00")(ev)} module is already deployed.`, "• X2 EVENT •");
+              global.loading.err(`${chalk.hex('#ff7100')(``)} ${chalk.hex("#FFFF00")(ev)} module is already deployed.`, "• ALVI EVENT •");
               continue;
             }
             if (config.dependencies) {
@@ -408,7 +408,7 @@ function onBot({ models: botModel }) {
               await onLoad(eventData);
             }
             global.client.events.set(config.name, event);
-            global.loading(`${crayon(``)}successfully deployed ${chalk.blueBright(config.name)}`, "• X2 EVENT •");
+            global.loading(`${crayon(``)}successfully deployed ${chalk.blueBright(config.name)}`, "• ALVI EVENT •");
           }
           catch (err) {
             global.loading.err(`${chalk.hex("#ff0000")('')}${chalk.blueBright(ev)} failed with error : ${err.message}` + `\n`, "event");
@@ -418,9 +418,9 @@ function onBot({ models: botModel }) {
 
         }
       })();
-    console.log(chalk.blue(`\n` + `• X2 DEPLOYING X2BOT DATA •`));
-    global.loading(`${crayon(``)}deployed ${chalk.blueBright(`${global.client.commands.size}`)} commands and ${chalk.blueBright(`${global.client.events.size}`)} events`, "• X2 DATA •");
-    global.loading(`${crayon(``)}deployed time : ${chalk.blueBright(((Date.now() - global.client.timeStart) / 1000).toFixed() + 's')}`, "• X2 DATA •");
+    console.log(chalk.blue(`\n` + `• ALVI DEPLOYING ALVI-BOT DATA •`));
+    global.loading(`${crayon(``)}deployed ${chalk.blueBright(`${global.client.commands.size}`)} commands and ${chalk.blueBright(`${global.client.events.size}`)} events`, "• ALVI DATA •");
+    global.loading(`${crayon(``)}deployed time : ${chalk.blueBright(((Date.now() - global.client.timeStart) / 1000).toFixed() + 's')}`, "• ALVI DATA •");
     const listenerData = {};
     listenerData.api = loginApiData;
     listenerData.models = botModel;
@@ -444,10 +444,10 @@ function onBot({ models: botModel }) {
     authentication.Sequelize = Sequelize;
     authentication.sequelize = sequelize;
     const models = require('../system/database/model.js')(authentication);
-    logger(`deployed ${chalk.blueBright('database')} system`, "• X2 DATABASE   •");
-    logger(`deploying ${chalk.blueBright('login')} system`, "• X2 LOGIN 	    •")
+    logger(`deployed ${chalk.blueBright('database')} system`, "• ALVI DATABASE   •");
+    logger(`deploying ${chalk.blueBright('login')} system`, "• ALVI LOGIN 	    •")
     const botData = {};
     botData.models = models;
     onBot(botData);
-  } catch (error) { logger(`can't deploy ${chalk.blueBright('database')} system`, "•X2 FAILED    •") }
+  } catch (error) { logger(`can't deploy ${chalk.blueBright('database')} system`, "• ALVI FAILED    •") }
 })();
